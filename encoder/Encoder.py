80702321adcc38e64a097e33bf7cb036bcb8138e
zrz <72608995+9898zy998@users.noreply.github.com>
2024-12-16 22:14:32 +0800

    Add files via upload

--- encoder/Encoder.py
+++ encoder/Encoder.py
@@ -1,16 +1,18 @@
 import torch.nn as nn
-from .utils import MultiHeadAttention, FeedForward
+from utils import MultiHeadAttention, PositionWiseFeedForward
 class EncoderLayer(nn.Module):
-    def __init__(self,d_model, num_heads, d_hidden,is_drop = True, drop = 0.1):
+    def __init__(self,d_model, num_heads, d_hidden, dropout):
         super().__init__()
         self.attn = MultiHeadAttention(d_model, num_heads)
         self.norm1 = nn.LayerNorm(d_model)
-        self.feed_forward = FeedForward(d_model,d_hidden,is_drop, drop)
+        self.feed_forward = PositionWiseFeedForward(d_model,d_hidden)
+        self.dropout = nn.Dropout(dropout)
         self.norm2 = nn.LayerNorm(d_model)
 
     def forward(self, x):
-        attn_output = self.attn(x, x, x)
+        attn_output,_ = self.attn(x, x, x)
         x = self.norm1(x + attn_output)
+        x = self.dropout(x)
         f_output = self.feed_forward(x)
         x = self.norm2(x + f_output)
         return x
